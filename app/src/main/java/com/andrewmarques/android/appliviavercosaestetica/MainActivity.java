a4c4b4d40be2d18ee5beed3a39835676dd308210
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-12-25 22:13:11 -0300

    Implementation Slider
    foi adicionado dependencias para adicionar o slider ao projeto

--- app/src/main/java/com/andrewmarques/android/appliviavercosaestetica/MainActivity.java
+++ app/src/main/java/com/andrewmarques/android/appliviavercosaestetica/MainActivity.java
@@ -3,11 +3,54 @@ package com.andrewmarques.android.appliviavercosaestetica;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // // implementação do slider
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.slider1_intro)
+                .build()
+        );
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.slider1_intro)
+                .build()
+        );
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.slider1_intro)
+                .build()
+        );
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.slider1_intro)
+                .build()
+        );
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.slider1_intro)
+                .build()
+        );
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.slider1_intro)
+                .canGoForward(false) // impede o usuario de passar o slide
+                .build()
+        );
     }
 }
