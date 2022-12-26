8c82c2e0c79bcd462b9a878ec4ce0330f5275213
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-12-26 14:55:27 -0300

    Cadastre-se
    implementação de telas Login, Cadastro e Registro
    implementação de troca de telas

--- app/src/main/java/com/andrewmarques/android/appliviavercosaestetica/MainActivity.java
+++ app/src/main/java/com/andrewmarques/android/appliviavercosaestetica/MainActivity.java
@@ -1,7 +1,8 @@
 package com.andrewmarques.android.appliviavercosaestetica;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
@@ -48,9 +49,17 @@ public class MainActivity extends IntroActivity {
 
         addSlide( new FragmentSlide.Builder()
                 .background(android.R.color.white)
-                .fragment(R.layout.slider1_intro)
+                .fragment(R.layout.slider_registro)
                 .canGoForward(false) // impede o usuario de passar o slide
                 .build()
         );
+    }
+
+    public void redirecionar_cadastro_on_clicker (View view ) {
+        startActivity(new Intent(this, Cadastro.class));
+    }
+
+    public void redirecionar_login_on_clicker ( View view ) {
+        startActivity(new Intent(this, Login.class));
     }
 }
